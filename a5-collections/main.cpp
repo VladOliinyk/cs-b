0987835a4c2ee4d067cce0db2dc69277738e347f
Vlad <vladokoliynyk@gmail.com>
2016-07-09 13:52:50 +0300

    Tests improvements.

--- a5-collections/main.cpp
+++ a5-collections/main.cpp
@@ -4,6 +4,12 @@
 #include "myList.h"
 #include <vector>
 
+#define stackTest_simple false
+#define stackTest_overloading false
+
+#define listTest_simple false
+#define listTest_overloading false
+
 
 using namespace std;
 
@@ -15,45 +21,78 @@ void printStackInfo(MyStack<int>* localStack) {
 
 void testMyStack() {
     ///// simple stack tests ///// start /////
-    /*
-    MyStack<int>* myIntStack = new MyStack<int>();
-    printStackInfo(myIntStack);
-    for (int i = 0; i < 10; i++) {
-        myIntStack->push(i);
-        cout << endl;
+    if (stackTest_simple) {
+        MyStack<int>* myIntStack = new MyStack<int>();
         printStackInfo(myIntStack);
+        for (int i = 0; i < 10; i++) {
+            myIntStack->push(i);
+            cout << endl;
+            printStackInfo(myIntStack);
+        }
+        for (int i = 0; i < 10; i++) {
+            myIntStack->pop();
+            cout << endl;
+            printStackInfo(myIntStack);
+        }
     }
-
-
-    for (int i = 0; i < 10; i++) {
-        myIntStack->pop();
-        cout << endl;
-        printStackInfo(myIntStack);
-    }*/
     ///// simple stack tests ///// end /////
 
-
     ///// is overloading = work ? ///// start /////
-    MyStack<int>* myStack = new MyStack<int>();
-    cout << myStack << endl;
-    myStack->push(1);
-    myStack->push(2);
-    myStack->push(3);
-    cout << myStack << endl;
+    if (stackTest_overloading) {
+        MyStack<int>* myStack = new MyStack<int>();
+        cout << "before filling: " << endl
+             << "myStack  " << myStack << "   size=" << myStack->getSize() << endl;
+        myStack->push(0);
+        myStack->push(1);
+        myStack->push(2);
+        myStack->push(3);
+        myStack->push(4);
+        myStack->push(5);
+        cout << " after filling: " << endl
+             << "myStack  " << myStack << "   size=" << myStack->getSize() << endl << endl;
 
-    MyStack<int>* myStack2 = new MyStack<int>();
-    cout << myStack2 << endl;
-    myStack2 = myStack;
-    cout << myStack2 << endl;
+        MyStack<int>* myStack2 = new MyStack<int>();
+        cout << "before filling: " << endl
+             << "myStack2 " << myStack2 << "   size=" << myStack2->getSize() << endl;
+        myStack2->push(10);
+        myStack2->push(11);
+        myStack2->push(12);
+        cout << " after filling: " << endl
+             << "myStack2 " << myStack2 << "   size=" << myStack2->getSize() << endl << endl;
+
+
+        cout << "before 'myStack2 = myStack': " << endl
+             << "myStack  " << myStack << "   size=" << myStack->getSize() << endl
+             << "myStack2 " << myStack2 << "   size=" << myStack2->getSize() << endl << endl;
+        myStack2->operator=(*myStack);
+        cout << " after 'myStack2 = myStack': " << endl
+             << "myStack  " << myStack << "   size=" << myStack->getSize() << endl
+             << "myStack2 " << myStack2 << "   size=" << myStack2->getSize() << endl;
+
+        MyStack<int>* myStack3 = new MyStack<int>();
+        cout << "before 'myStack3 = myStack2': " << endl
+             << "myStack2 " << myStack2 << "   size=" << myStack2->getSize() << endl
+             << "myStack3 " << myStack3 << "   size=" << myStack3->getSize() << endl;
+        myStack3 = myStack2;
+        myStack2->push(1231);
+        myStack2->push(1231);
+        myStack2->push(1231);
+        myStack2->push(1231);
+        cout << " after 'myStack3 = myStack2': " << endl
+             << "myStack2 " << myStack2 << "   size=" << myStack2->getSize() << endl
+             << "myStack3 " << myStack3 << "   size=" << myStack3->getSize() << endl;
+    }
     // перегрузка оператора = не работает
-    // оно просто копирует поинтер на стек
+    // оно просто копирует поинтер на другой стек
     ///// is overloading = work ? ///// end /////
 }
 
-/* !!! need to non privat variables !!! */
+
+// !!! need to non privat variables !!! //
 void printPrivatListInfo(MyList<int>* list) {
     cout << "list->head " << list->head << endl;
     cout << "list->tail " << list->tail << endl;
+    cout << "list->size " << list->getSize() << endl;
     if (list->head != NULL && list -> tail != NULL) {
         cout << "list->head->data " << list->head->data << endl;
         cout << "list->head->next " << list->head->next << endl;
@@ -66,79 +105,81 @@ void printPrivatListInfo(MyList<int>* list) {
     }
 }
 
+
 void testMyList() {
 
     ///// push/pop simple test ? ///// start /////
-    /*
-    MyList<int> *list = new MyList<int>();
-    printPrivatListInfo(list);
+    if (listTest_simple) {
+        MyList<int> *listS = new MyList<int>();
+        printPrivatListInfo(listS);
 
-    for (int i=0; i < 5; i++) {
-        cout << endl;
-        cout << "push_back(" << i*2 << ")" << " ... ";
-        list->push_back(i*2);
-        cout << "OK" << endl;
-        printPrivatListInfo(list);
+        for (int i=0; i < 5; i++) {
+            cout << endl;
+            cout << "push_back(" << i*2 << ")" << " ... ";
+            listS->push_back(i*2);
+            cout << "OK" << endl;
+            printPrivatListInfo(listS);
+        }
+
+        for (int i=0; i < 6; i++) {
+            cout << endl;
+            cout << "pop_front #" << i << " ... ";
+            listS->pop_front();
+            cout << "OK" << endl;
+            printPrivatListInfo(listS);
+        }
     }
-
-    for (int i=0; i < 6; i++) {
-        cout << endl;
-        cout << "pop_front #" << i << " ... ";
-        list->pop_front();
-        cout << "OK" << endl;
-        printPrivatListInfo(list);
-    } */
     ///// push/pop simple test ? ///// end /////
 
 
     ///// is overloading = work ? ///// start /////
-    /*
-    MyList<int> *list1 = new MyList<int>();
-    for (int i = 0; i < 10; i++) {
-        list1->push_back(i+10);
-        //cout << "1#" << i << " = " << list1->tail->data << endl;
+    if (listTest_overloading) {
+        MyList<int> *list1 = new MyList<int>();
+        cout << "----- LIST 1 BEFORE FILLING: -----" << endl;
+        printPrivatListInfo(list1);
+        list1->printList();
+        cout << "----- ----- ---------- ----- -----" << endl;
+
+        for (int i = 0; i < 10; i++) {
+            list1->push_back(i+10);
+        }
+
+        cout << "----- LIST 1  AFTER FILLING: -----" << endl;
+        printPrivatListInfo(list1);
+        list1->printList();
+        cout << "----- ----- ---------- ----- -----" << endl;
+
+        MyList<int> *list2 = new MyList<int>();
+        cout << "----- LIST 2 BEFORE FILLING: -----" << endl;
+        printPrivatListInfo(list2);
+        list2->printList();
+        cout << "----- ----- ---------- ----- -----" << endl;
+
+        for (int i = 0; i < 5; i++) {
+            list2->push_back(i+50);
+        }
+
+        cout << "----- LIST 2  AFTER FILLING: -----" << endl;
+        printPrivatListInfo(list2);
+        list2->printList();
+        cout << "----- ----- ---------- ----- -----" << endl;
+
+
+        cout << "----- LIST 1&2 BEFORE list2 = list1 : -----" << endl;
+        list1->printList();
+        list2->printList();
+        cout << "----- ----- ----- --- --- ----- ----- -----" << endl;
+
+        list2->operator =(*list1);
+        list2 = list1;
+        list1->head->data=88;
+
+        cout << "----- LIST 1&2  AFTER list2 = list1 : -----" << endl;
+        list1->printList();
+        list2->printList();
+        cout << "----- ----- ----- --- --- ----- ----- -----" << endl;
     }
-
-    MyList<int> *list2 = new MyList<int>();
-    for (int i = 0; i < 10; i++) {
-        list1->push_back(i+50);
-        //cout << "2#" << i << " = " << list1->tail->data << endl;
-    }
-
-    MyList<int> *list3 = new MyList<int>();
-
-    cout << "list1 = " << list1 << endl <<
-            "list2 = " << list2 << endl <<
-            "list3 = " << list3 << endl << endl;
-
-    list3=list1;
-
-    MyList<int> *list4 = list1;
-
-    cout << "list1 = " << list1 << endl <<
-            "list2 = " << list2 << endl <<
-            "list3 = " << list3 << endl <<
-            "list4 = " << list4 << endl << endl;
-    // я хз, оно просто копирует поинтер на head и все
-
-    list3->pop_front();
-    list3->pop_back();
-
-    cout << "list1 = " << list1 << endl <<
-            "list2 = " << list2 << endl <<
-            "list3 = " << list3 << endl <<
-            "list4 = " << list4 << endl << endl;
-
-    list1->clear();
-    list3->pop_front();
-    list3->pop_back();
-    //да, он реально просто копирует указатель и игнорирует мою перегрузку оператора =
-    cout << "list1 = " << list1 << endl <<
-            "list2 = " << list2 << endl <<
-            "list3 = " << list3 << endl <<
-            "list4 = " << list4 << endl;
-    */
-    ///// is overloading = work ? ///// end /////
+    ///// is overloading = work ? ///// end ///// - nope
 
 
     ///// direction test ///// start /////
@@ -164,11 +205,11 @@ void testMyList() {
     ///// direction test ///// end /////
 }
 
-
+/* Main function */
 int main()
 {
-    //testMyStack();
-    //testMyList();
+    testMyStack();
+    testMyList();
     return 0;
 }
 
